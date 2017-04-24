6f611f016e70c4956687875e2f2ba4bfc7d91bdf
resat <resat.gafarli@gmail.com>
2017-04-24 20:00:19 +0300

    State property assignmet are corrected according to dynamic state machine construction unit version

--- LanguageShadowingCenter/src/QtUnitTests/tst_appstatemachine.h
+++ LanguageShadowingCenter/src/QtUnitTests/tst_appstatemachine.h
@@ -58,65 +58,82 @@ private Q_SLOTS:
     /*State machine creation and state switch test, object property assigmnet test*/
     void stateMachinePropertyAssignmentTest()
     {
-  /*      LSCApplicationStateMachine stateMachine;
+        LSCApplicationStateMachine stateMachine;
+        stateMachine.addStatesTransition("s1","s2");
+        stateMachine.addStatesTransition("s2","s3");
+        stateMachine.addStatesTransition("s3","s1");
+        stateMachine.addStatesTransition("s1","s3");
+        stateMachine.addStatesTransition("s3","s2");
+        stateMachine.addStatesTransition("s2","s1");
+        stateMachine.setInitialState("s1");
+
         QObject obj1,obj2,obj3;
         obj1.setProperty("property","INITIAL");
         obj2.setProperty("property","INITIAL");
         obj3.setProperty("property","INITIAL");
 
-        stateMachine.setObjectStateProperty(obj1,"property");
-        stateMachine.setObjectStateProperty(obj2,"property");
-        stateMachine.setObjectStateProperty(obj3,"property");
+        stateMachine.setObjectStateProperty(obj1,"s1","property","STATE1");
+        stateMachine.setObjectStateProperty(obj1,"s2","property","STATE2");
+        stateMachine.setObjectStateProperty(obj1,"s3","property","STATE3");
+
+        stateMachine.setObjectStateProperty(obj2,"s1","property","STATE1");
+        stateMachine.setObjectStateProperty(obj2,"s2","property","STATE2");
+        stateMachine.setObjectStateProperty(obj2,"s3","property","STATE3");
+
+        stateMachine.setObjectStateProperty(obj3,"s1","property","STATE1");
+        stateMachine.setObjectStateProperty(obj3,"s2","property","STATE2");
+        stateMachine.setObjectStateProperty(obj3,"s3","property","STATE3");
+
 
         stateMachine.start();
         QVERIFY(stateMachine.checkStarted());
-        QVERIFY(stateMachine.checkIfInState1());
+        QVERIFY(stateMachine.checkIfInState("s1"));
         QCOMPARE(obj1.property("property"),QVariant("STATE1"));
         QCOMPARE(obj2.property("property"),QVariant("STATE1"));
         QCOMPARE(obj3.property("property"),QVariant("STATE1"));
 
         //Forward
-        stateMachine.switchToState("fromState1toState2");QCoreApplication::processEvents();
-        QVERIFY(stateMachine.checkIfInState2());
+        stateMachine.switchToState("s1","s2");QCoreApplication::processEvents();
+        QVERIFY(stateMachine.checkIfInState("s2"));
         QCOMPARE(obj1.property("property"),QVariant("STATE2"));
         QCOMPARE(obj2.property("property"),QVariant("STATE2"));
         QCOMPARE(obj3.property("property"),QVariant("STATE2"));
 
 
-        stateMachine.switchToState("fromState2toState3");QCoreApplication::processEvents();
-        QVERIFY(stateMachine.checkIfInState3());
+        stateMachine.switchToState("s2","s3");QCoreApplication::processEvents();
+        QVERIFY(stateMachine.checkIfInState("s3"));
         QCOMPARE(obj1.property("property"),QVariant("STATE3"));
         QCOMPARE(obj2.property("property"),QVariant("STATE3"));
         QCOMPARE(obj3.property("property"),QVariant("STATE3"));
 
-        stateMachine.switchToState("fromState3toState1");QCoreApplication::processEvents();
-        QVERIFY(stateMachine.checkIfInState1());
+        stateMachine.switchToState("s3","s1");QCoreApplication::processEvents();
+        QVERIFY(stateMachine.checkIfInState("s1"));
         QCOMPARE(obj1.property("property"),QVariant("STATE1"));
         QCOMPARE(obj2.property("property"),QVariant("STATE1"));
         QCOMPARE(obj3.property("property"),QVariant("STATE1"));
 
 
         //Backward
-        stateMachine.switchToState("fromState1toState3");QCoreApplication::processEvents();
-        QVERIFY(stateMachine.checkIfInState3());
+        stateMachine.switchToState("s1","s3");QCoreApplication::processEvents();
+        QVERIFY(stateMachine.checkIfInState("s3"));
         QCOMPARE(obj1.property("property"),QVariant("STATE3"));
         QCOMPARE(obj2.property("property"),QVariant("STATE3"));
         QCOMPARE(obj3.property("property"),QVariant("STATE3"));
 
 
-        stateMachine.switchToState("fromState3toState2");QCoreApplication::processEvents();
-        QVERIFY(stateMachine.checkIfInState2());
+        stateMachine.switchToState("s3","s2");QCoreApplication::processEvents();
+        QVERIFY(stateMachine.checkIfInState("s2"));
         QCOMPARE(obj1.property("property"),QVariant("STATE2"));
         QCOMPARE(obj2.property("property"),QVariant("STATE2"));
         QCOMPARE(obj3.property("property"),QVariant("STATE2"));
 
 
-        stateMachine.switchToState("fromState2toState1");QCoreApplication::processEvents();
-        QVERIFY(stateMachine.checkIfInState1());
+        stateMachine.switchToState("s2","s1");QCoreApplication::processEvents();
+        QVERIFY(stateMachine.checkIfInState("s1"));
         QCOMPARE(obj1.property("property"),QVariant("STATE1"));
         QCOMPARE(obj2.property("property"),QVariant("STATE1"));
         QCOMPARE(obj3.property("property"),QVariant("STATE1"));
-*/
+
     }
 
 };
