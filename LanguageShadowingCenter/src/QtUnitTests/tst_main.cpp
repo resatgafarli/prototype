90524e6a783b781c80f0e8ceb427a5910bd64bc3
Reşat Gafarlı <resat.gafarli@gmail.com>
2017-04-26 20:41:43 +0200

    Mediator pattern test isolation example is implemented.

--- LanguageShadowingCenter/src/QtUnitTests/tst_main.cpp
+++ LanguageShadowingCenter/src/QtUnitTests/tst_main.cpp
@@ -7,6 +7,7 @@ License: GPL-3.0
 
 #include <QString>
 #include <QtTest>
+#include <tst_example_mediator_isolation.h>
 #include <tst_appstatemachine.h>
 #include <tst_episodpreparation.h>
 #include <tst_episodepreparationsm.h>
@@ -18,8 +19,9 @@ int main(int argc, char** argv)
    { tst_CustomEventTypeGenerator tc; QTest::qExec(&tc, argc, argv);}
    { tst_AppStateMachine tc; QTest::qExec(&tc, argc, argv);}
    { tst_EpisodePreparationSM tc; QTest::qExec(&tc, argc, argv);}
-
-   //{ EpisdoPreparationTests tc; QTest::qExec(&tc, argc, argv);}
+   { EpisdoPreparationTests tc; QTest::qExec(&tc, argc, argv);}
+   //{ tst_TestMediatorClass tc; QTest::qExec(&tc, argc, argv);}
+   //{ tst_TestMediatorControlClass tc;QTest::qExec(&tc, argc, argv);}
 
    QMetaObject::invokeMethod(&a, "quit", Qt::QueuedConnection);
    return a.exec();
