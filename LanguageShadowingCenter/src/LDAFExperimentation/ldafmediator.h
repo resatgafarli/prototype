fa42f4b2e01336d9c61d4b4542bbb11e7bbd1401
resat <resat.gafarli@gmail.com>
2017-05-22 19:06:34 +0300

    Example command pattern classes are added.

--- LanguageShadowingCenter/src/LDAFExperimentation/ldafmediator.h
+++ LanguageShadowingCenter/src/LDAFExperimentation/ldafmediator.h
@@ -9,6 +9,47 @@ License: GPL-3.0
 
 #include <QObject>
 
+
+class LDAFUri{
+//TODO: BIG 5 of this class should be implemented
+private:
+    QString m_method;
+    QString m_resourceTree;
+    QString m_resourceName;
+    QString m_operation;
+    QList<QPair<QString,QString>> m_operationParsVals;
+
+public:
+    explicit LDAFUri(QString method="", QString resource_tree="", QString resoruce_name="", QString operation=""):
+        m_method(method),
+        m_resourceTree(resource_tree),
+        m_resourceName(resoruce_name),
+        m_operation(operation){}
+
+    void setOperationParsVals(QString par, QString val){
+        m_operationParsVals.append(QPair<QString,QString>(par,val));
+    }
+};
+
+class LDAFCommand{
+    virtual void executeMethod() =0;
+};
+
+class LDAFRequest:public LDAFCommand {
+
+};
+
+class LDAFResponse:public LDAFCommand {
+
+};
+
+
+class LDAFProtocol {
+public:
+
+
+};
+
 class LDAFMediator : public QObject
 {
     Q_OBJECT
